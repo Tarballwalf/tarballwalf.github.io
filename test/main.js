1b1295fd8572b702be6a0523d9b96a6fc6478265
Tarballwalf <nicu.pilt@gmail.com>
2021-01-01 18:54:42 +0200

    oof

--- test/main.js
+++
@@ -1,9 +0,0 @@
-var link = document.createElement('meta');
-link.setAttribute('property', 'og:title');
-link.content = document.title;
-document.getElementsByTagName('head')[0].appendChild(link)
-
-var link = document.createElement('meta');
-link.setAttribute('property', 'title');
-link.content = document.title;
-document.getElementsByTagName('head')[0].appendChild(link);
