e18255f8a8f94ca49c9489a680be2265eb7b2972
Tarballwalf <nicu.pilt@gmail.com>
2021-03-11 16:37:45 +0200

    fixed the spitting js error

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-setInterval(() => {
-    document.getElementById('time2').innerHTML = moment.tz('Europe/Bucharest').format('dddd, MMMM Do   HH:mm (hh:mm A) z')
-}, 300)
-
 var toggler = document.getElementsByClassName("caret");
-          var i;
-          
-          for (i = 0; i < toggler.length; i++) {
-            toggler[i].addEventListener("click", function() {
-              this.parentElement.querySelector(".nested").classList.toggle("active");
-              this.classList.toggle("caret-down");
-            });
-          }
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
